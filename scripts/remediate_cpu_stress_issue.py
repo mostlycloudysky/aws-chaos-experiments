7ba392c797daf9ef72d4a84804f08a8d9a0927ae
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2024-07-27 11:57:24 -0400

    remediate the cpu stress

--- scripts/remediate_cpu_stress_issue.py
+++ scripts/remediate_cpu_stress_issue.py
@@ -27,13 +27,29 @@ def reopen_issue():
         print(f"Failed to reopen issue #{issue_number}: {response.text}")
 
 
+def run_ssm_document(instance_id):
+    ssm = boto3.client("ssm", region_name=os.getenv("AWS_REGION"))
+
+    document_name = "AWS-RunPowerShellScript"
+    commands = ["Get-Job | Stop-Job -Force", "Get-Job | Remove-Job -Force"]
+
+    response = ssm.send_command(
+        InstanceIds=[instance_id],
+        DocumentName=document_name,
+        Parameters={"commands": commands},
+    )
+    command_id = response["Command"]["CommandId"]
+    print(
+        f"Sent SSM command to stop CPU stress on instance {instance_id}: {command_id}"
+    )
+
+
 def remediate_cpu_stress_issue():
     issue_body = os.getenv("ISSUE_BODY")
     try:
         instance_id = parse_body(issue_body)
         print(f"Instance ID to remediate {instance_id}")
-
-        # Run the SSM document on that
+        run_ssm_document(instance_id)
 
     except Exception as e:
         print(f"Error during remediation {e}")
