c8349907b9b73281d97051814c8d81e81068025f
Sandeep K Yaramchitti <ysandeepkumar88@gmail.com>
2024-07-28 08:22:37 -0400

    test

--- scripts/detect_network_latency_issues.py
+++ scripts/detect_network_latency_issues.py
@@ -1,11 +1,10 @@
 import os
 import boto3
 import requests
-from datetime import datetime, timedelta
 
 
-def detect_network_latency():
-    print("Detecting Network Latency issues...")
+def detect_alarm_state():
+    print("Detecting NetworkIn alarm state...")
     cloudwatch = boto3.client("cloudwatch", region_name=os.getenv("AWS_REGION"))
 
     alarm_name = os.getenv("CLOUDWATCH_ALARM_NAME")
@@ -19,12 +18,10 @@ def detect_network_latency():
     alarm_state = alarms[0]["StateValue"]
 
     if alarm_state == "ALARM":
-        instance_id = alarms[0]["Dimensions"][0][
-            "Value"
-        ]  # Assuming the alarm is set on a single EC2 instance
+        instance_id = alarms[0]["Dimensions"][0]["Value"]
         create_github_issue(instance_id)
     else:
-        print("No CPU stress issues detected.")
+        print("No NetworkIn alarm issues detected.")
 
 
 def create_github_issue(instance_id):
@@ -32,17 +29,17 @@ def create_github_issue(instance_id):
     token = os.getenv("GITHUB_TOKEN")
     url = f"https://api.github.com/repos/{repo}/issues"
     headers = {"Authorization": f"token {token}"}
-    issue_title = f"EC2 Instance {instance_id} Network Latency Detected"
-    issue_body = f"The CloudWatch alarm for EC2 instance {instance_id} is in the ALARM state, indicating high Network Letency. Remediation action is required."
+    issue_title = f"EC2 Instance {instance_id} NetworkIn Alarm Triggered"
+    issue_body = f"The NetworkIn alarm for EC2 instance {instance_id} has been triggered. Remediation action is required."
     issue = {"title": issue_title, "body": issue_body}
     response = requests.post(url, headers=headers, json=issue)
     if response.status_code == 201:
         print(f"GitHub issue created for instance {instance_id}")
     else:
         print(
-            f"Failed to create GitHub issue for instance {instance_id}: {response.text}"
+            f"Failed to create GitHub issue for instance {instance_id}: {response.content}"
         )
 
 
 if __name__ == "__main__":
-    detect_network_latency()
+    detect_alarm_state()
